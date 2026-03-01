e672ba981d6c92667532d84cebfe40135d1da470
mohamednabil00000 <mohamednabil00000@gmail.com>
2026-03-01 21:11:50 +0200

    Add docker

---
+++ docker/startup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /app/tmp/pids/server.pid
+
+# Wait for DB services
+#sh ./docker/wait-for-services.sh
+
+# Prepare DB (Migrate - If not? Create db & Migrate)
+sh /usr/bin/prepare-db.sh
+
+# # Prepare Cron Jobs
+# sh /usr/bin/prepare-cronjob.sh
+
+# Pre-comple app assets
+#sh ./docker/asset-pre-compile.sh
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
